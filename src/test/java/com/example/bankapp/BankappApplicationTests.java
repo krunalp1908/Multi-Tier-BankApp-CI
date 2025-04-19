cebcc1a94d0d27127f448fb2691024f009c3ed74
KrunalParekh <157247364+krunalp1908@users.noreply.github.com>
2025-04-19 13:30:39 +0530

    Update BankappApplicationTests.java

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Activates the test config!
 class BankappApplicationTests {
 
 	@Test
